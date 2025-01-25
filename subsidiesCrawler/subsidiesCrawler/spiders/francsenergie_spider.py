9b71c855d3ef7944fe93a419f4b7d29a81393a86
Saddam <s.satouyev@solarsplit.com>
2025-01-25 17:19:25 +0100

    [main] filter fields

--- subsidiesCrawler/subsidiesCrawler/spiders/francsenergie_spider.py
+++ subsidiesCrawler/subsidiesCrawler/spiders/francsenergie_spider.py
@@ -4,7 +4,7 @@ import os
 
 class MySpider(scrapy.Spider):
     name = "francsenergie_spider"
-    start_urls = ["https://www.francsenergie.ch/fr/1261-Le-Vaud/building/personal"]
+    start_urls = ["https://www.francsenergie.ch/fr/2000-Neuchatel/building/personal"]
 
     def parse(self, response):
         # Extraction des paragraphes (exemple)
@@ -17,6 +17,13 @@ class MySpider(scrapy.Spider):
         if div:
             # Chargement du JSON
             json_data = json.loads(div)
+            
+            # Filtrer les champs pour ne garder que ceux avec 'name' == 'Production d'électricité'
+            filtered_fields = [field for field in json_data['town']['fields'] if field['name'] == 'Production d’électricité']
+            print(filtered_fields)
+            print("🔥🔥🔥🔥🔥")
+            # Mettre à jour les champs avec les champs filtrés
+            json_data['town']['fields'] = filtered_fields
             
             # Chemin du fichier de sortie
             output_file = os.path.join(self.settings.get('FILES_STORE', ''), 'subsidies_data.json')
